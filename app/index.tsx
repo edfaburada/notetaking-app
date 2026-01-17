d7c9098922714291e76c0e5a866c4429a8901a92
kragodo <kzragodo.student@asiancollege.edu.ph>
2026-01-18 07:50:13 +0800

    correct routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useRouter } from 'expo-router';
-import { supabase } from '../supabase';
+import { supabase } from '@/supabase';
 
 export default function Index() {
   const router = useRouter();
@@ -8,8 +8,8 @@ export default function Index() {
   useEffect(() => {
     const checkSession = async () => {
       const { data } = await supabase.auth.getSession();
-      if (data.session) router.replace({ pathname: '/dashboard/index' });
-      else router.replace({ pathname: '/auth/login' });
+      if (data.session) router.replace({ pathname: '../index' });
+      else router.replace({ pathname: '../login' });
     };
 
     checkSession();
