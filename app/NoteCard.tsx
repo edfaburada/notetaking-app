2e79844cb3b67d25dcb9caeee8550f031a7cf1fd
kragodo <kzragodo.student@asiancollege.edu.ph>
2026-01-21 17:37:16 +0800

    note card margin fix

--- app/NoteCard.tsx
+++ app/NoteCard.tsx
@@ -38,6 +38,8 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 5,
     elevation: 3,
+    marginLeft: 15,
+    marginRight: 15,
   },
   image: {
     width: '100%',
